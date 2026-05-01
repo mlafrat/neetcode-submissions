de05d772261175f865d5dd10ce225f2734e3fa7c
mlafrat <146100054+mlafrat@users.noreply.github.com>
2026-04-30 20:35:44 -0500

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+
+        if(s.length !== t.length) {
+            return false;
+        }
+
+        const sMap = new Map();
+        const tMap = new Map();
+
+        for(let i = 0; i < s.length; i++) {
+            sMap.set(s[i], (sMap.get(s[i]) || 0) + 1);
+            tMap.set(t[i], (tMap.get(t[i]) || 0) + 1);
+        }
+
+        for(let [key, value] of sMap) {
+            if(tMap.get(key) == undefined){
+                return false;
+            }
+            if(tMap.get(key) !== value){
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
